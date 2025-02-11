208d117bbdaa69b86cc0db0e9aa3f32c703facdd
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2025-02-11 21:04:32 +0000

    script 4 setup

---
+++ scripts/setup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -eux
+
+export BUILDX_PROGRESS=plain
+export BUILDKIT_PROGRESS=plain
+
+if [ -z "$(command -v docker)" ]; then
+    echo "Docker is not installed. Please install Docker and try again."
+    exit 1
+fi
+
+if [ -z "$(command -v cartesi)" ]; then
+    npm i -g @cartesi/cli
+    sudo docker run --privileged --rm tonistiigi/binfmt:riscv || true
+    cartesi doctor
+fi
+
+if [ -z "$(command -v foundryup)" ]; then
+    curl -L https://foundry.paradigm.xyz | bash
+    source /home/codespace/.bashrc
+    foundryup
+fi
+
+# Check if there are any running containers before trying to stop them
+if [ ! -z "$(docker ps -q)" ]; then
+    echo "Stopping running containers..."
+    docker stop $(docker ps -q)
+fi
+
+docker buildx rm --force --all-inactive
+docker buildx prune --all --force && docker system prune --volumes --force
+
+echo "Install Rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
+
+echo "Install coprocessor cli"
+cargo install cartesi-coprocessor
+
