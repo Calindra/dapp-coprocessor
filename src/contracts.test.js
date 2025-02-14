199c4641688940907f5c63f9abef44b0e8c642c7
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-02-14 03:32:50 -0300

    feat: add match creation functionality and update contract tests for team creation

--- src/contracts.test.js
+++ src/contracts.test.js
@@ -1,16 +1,27 @@
 import { it, before } from "node:test";
 import assert from "node:assert";
-import { http, createPublicClient, toHex } from "viem";
+import { mnemonicToAccount } from "viem/accounts";
+import { http, createPublicClient, createWalletClient, toHex } from "viem";
 import { foundry } from "viem/chains";
 import { myContractAbi } from "./rollups.js";
 
 /** @type {ReturnType<(typeof createPublicClient)>} */
 let client;
+/** @type {ReturnType<(typeof createWalletClient)>} */
+let wallet;
+const account = mnemonicToAccount(
+  "test test test test test test test test test test test junk"
+);
 
 before(async () => {
   client = createPublicClient({
     chain: foundry,
     transport: http(),
+  });
+  wallet = createWalletClient({
+    chain: foundry,
+    transport: http(),
+    account,
   });
 });
 
@@ -35,19 +46,24 @@ it("should simulate team creation and retrieval", async () => {
     address: dappAddress,
     abi: myContractAbi,
     eventName: "TeamCreated",
+    onError: (error) => console.error(error),
     onLogs: (logs) => {
       console.log("TeamCreated event logs:", logs);
     },
   });
 
-  const { result: teamId } = await client.simulateContract({
+  const { result: teamId, request } = await client.simulateContract({
     address: dappAddress,
     abi: myContractAbi,
     functionName: "createTeam",
     args: team,
   });
   assert.ok(teamId, "Team ID should be returned");
-  console.log("Team ID:", teamId);
+  // @ts-ignore
+  const hash = await wallet.writeContract(request);
+  assert.ok(hash, "Transaction hash should be returned");
+  const teamIdHex = toHex(teamId);
+  console.log("Team ID:", teamIdHex);
   assert.strictEqual(
     teamId,
     93425661894681441242860048108082761268965539453039422627886306835243180765084n
