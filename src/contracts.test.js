2dd49096396bb4d61a31a5916aeb552ecb45e523
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-02-14 04:41:09 -0300

    feat: enhance contract tests with event logging and improve team creation simulation

--- src/contracts.test.js
+++ src/contracts.test.js
@@ -1,4 +1,5 @@
-import { it, before } from "node:test";
+import { it, before, after } from "node:test";
+import { setTimeout } from "node:timers/promises";
 import assert from "node:assert";
 import { mnemonicToAccount } from "viem/accounts";
 import { http, createPublicClient, createWalletClient, toHex } from "viem";
@@ -9,9 +10,14 @@ import { myContractAbi, footballTeamAbi } from "./rollups.js";
 let client;
 /** @type {ReturnType<(typeof createWalletClient)>} */
 let wallet;
+/** @type {ReturnType<(typeof client)["watchContractEvent"]>} */
+let unwatch;
 const account = mnemonicToAccount(
   "test test test test test test test test test test test junk"
 );
+
+const footballAddress = "0x1429859428C0aBc9C2C47C8Ee9FBaf82cFA0F20f";
+const dappAddress = "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07";
 
 before(async () => {
   client = createPublicClient({
@@ -23,16 +29,29 @@ before(async () => {
     transport: http(),
     account,
   });
+
+  unwatch = client.watchContractEvent({
+    address: dappAddress,
+    abi: myContractAbi,
+    onError: (error) => {
+      console.error("Error Events Logs:", error);
+    },
+    onLogs: (logs) => {
+      console.log("Event logs:", logs);
+    },
+  });
+});
+
+after(async () => {
+  await setTimeout(10_000);
+  unwatch();
 });
 
 it("should pass", async () => {
   assert.strictEqual(1, 1);
 });
 
-const footballAddress = "0x1429859428C0aBc9C2C47C8Ee9FBaf82cFA0F20f";
-const dappAddress = "0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07";
-
-it("should simulate team creation and retrieval", async () => {
+it("should simulate team creation", async () => {
   const team = [
     "MyStarTeam",
     { name: "Rogerio Ceni", level: 2 },
@@ -42,17 +61,6 @@ it("should simulate team creation and retrieval", async () => {
   ];
 
   // Simulate creating a team
-  client.watchContractEvent({
-    address: dappAddress,
-    abi: myContractAbi,
-    onError: (error) => {
-      console.error("Error Events Logs:", error)
-    },
-    onLogs: (logs) => {
-      console.log("Event logs:", logs);
-    },
-  });
-
   const { result: teamId, request } = await client.simulateContract({
     address: dappAddress,
     abi: myContractAbi,
@@ -64,19 +72,54 @@ it("should simulate team creation and retrieval", async () => {
   // @ts-ignore
   const hash = await wallet.writeContract(request);
   assert.ok(hash, "Transaction hash should be returned");
+  console.log("Transaction hash:", hash);
   const teamIdHex = toHex(teamId);
   // 0xce8d0cfd5165bac9ab98b7f4ac03ed1af41de1cb88502920a07b364cbe542f9c
-  console.log("Team ID:", teamIdHex, "\nTransaction hash:", hash);
+  console.log("Team ID:", teamIdHex);
   assert.strictEqual(
     teamId,
     93425661894681441242860048108082761268965539453039422627886306835243180765084n
   );
-  // const { result: teamName } = await client.simulateContract({
-  //   address: footballAddress,
-  //   abi: footballTeamAbi,
-  //   functionName: "getTeamName",
-  //   args: [teamId],
-  // });
+  const teamBigId = BigInt(teamId);
+  const { result: teamName } = await client.simulateContract({
+    address: footballAddress,
+    abi: footballTeamAbi,
+    functionName: "getTeamName",
+    args: [teamBigId],
+    account,
+  });
 
-  // assert.strictEqual("MyStarTeam", teamName); // Fixing the assertion to match created team's name
+  assert.strictEqual("MyStarTeam", teamName);
+});
+
+it.skip("should simulate team creation and retrieval", async () => {
+  const team = [
+    "MyCartesiTeam",
+    { name: "Garry", level: 2 },
+    [{ name: "Oshiro", level: 4 }],
+    [{ name: "Ghiggino", level: 7 }],
+    [{ name: "Bruno", level: 11 }],
+  ];
+
+  // @ts-ignore
+  const { result } = await client.simulateCalls({
+    account,
+    calls: [
+      {
+        to: dappAddress,
+        abi: myContractAbi,
+        functionName: "createTeam",
+        args: team,
+      },
+      {
+        to: dappAddress,
+        abi: footballTeamAbi,
+        functionName: "getTeamName",
+        args: [0],
+      },
+    ],
+  });
+
+  assert.ok(result, "Team should be returned");
+  assert.strictEqual(1, 2);
 });
