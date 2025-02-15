3afe119b1f1d871b813604b9e1a1c2ef9ca2deae
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2025-02-15 09:37:18 -0300

    feat: add verifyBeacon from Garry jobs

--- src/game.js
+++ src/game.js
@@ -1,5 +1,20 @@
-import { encodeAbiParameters, toBytes, keccak256 } from "viem";
+import { encodeAbiParameters } from "viem";
 import seedrandom from "seedrandom";
+import {
+    verifyBeacon,
+} from 'drand-client/beacon-verification.js'
+
+const CHAIN_INFO = {
+    "public_key": "83cf0f2896adee7eb8b5f01fcad3912212c437e0073e911fb90022d3e760183c8c4b450b6a0a6c3ac6a5776a2d1064510d1fec758c921cc22b0e17e63aaf4bcb5ed66304de9cf809bd274ca73bab4af5a6e9c76a4bc09e76eae8991ef5ece45a",
+    "period": 3,
+    "genesis_time": 1692803367,
+    "hash": "52db9ba70e0cc0f6eaf7803dd07447a1f5477735fd3f661792ba94600c84e971",
+    "groupHash": "f477d5c89f21a17c863a7f937c6a6d15859414d2be09cd448d4279af331c5d3e",
+    "schemeID": "bls-unchained-g1-rfc9380",
+    "metadata": {
+        "beaconID": "quicknet"
+    }
+};
 
 const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
 console.log("HTTP rollup_server url is " + rollup_server);
@@ -27,6 +42,11 @@ async function emitNotice(data) {
 
 export async function runGame(match) {
     console.log(`Running a game`, { match })
+    const verifyResult = await verifyBeacon(CHAIN_INFO, match.beacon, match.beacon.round);
+    console.log('verifyResult', verifyResult);
+    if (!verifyResult) {
+        return
+    }
     const rnd = seedrandom(match.beacon.randomness)
     const tokenIds = [BigInt(match.teamA.goalkeeper.id)];
     const xpAmounts = [42n];
@@ -53,7 +73,6 @@ export async function runGame(match) {
     const goalsA = Math.floor(rnd() * teamGoalsA * 4.3);
     const goalsB = Math.floor(rnd() * teamGoalsB * 4.3);
     const uuidBytes = '0x' + match.reqId.replace(/-/g, "")
-    console.log({ uuidBytes })
     const encodedNotice = encodeAbiParameters(
         [
             { type: 'uint256[]' },
